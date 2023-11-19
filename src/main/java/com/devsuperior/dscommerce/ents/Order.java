1c6493e2dc5bb2bbfb20942cc6bfbefdcb4151fd
Caue Garcia <cauerg7@outlook.com>
2023-11-19 15:08:33 -0300

    payment

--- src/main/java/com/devsuperior/dscommerce/ents/Order.java
+++ src/main/java/com/devsuperior/dscommerce/ents/Order.java
@@ -1,9 +1,11 @@
 package com.devsuperior.dscommerce.ents;
 
 import java.time.Instant;
+import java.util.Objects;
 
 import com.devsuperior.dscommerce.ents.enums.OrderStatus;
 
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
@@ -11,6 +13,7 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToOne;
 import jakarta.persistence.Table;
 
 @Entity
@@ -29,5 +32,72 @@ public class Order {
 	@JoinColumn(name = "client_id")
 	private User client;
 	
-	
+	@OneToOne(mappedBy = "order", cascade = CascadeType.ALL)
+	public Payment payment;
+
+	public Order(Long id, Instant moment, OrderStatus status, User client, Payment payment) {
+		super();
+		this.id = id;
+		this.moment = moment;
+		this.status = status;
+		this.client = client;
+		this.payment = payment;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public Instant getMoment() {
+		return moment;
+	}
+
+	public void setMoment(Instant moment) {
+		this.moment = moment;
+	}
+
+	public OrderStatus getStatus() {
+		return status;
+	}
+
+	public void setStatus(OrderStatus status) {
+		this.status = status;
+	}
+
+	public User getClient() {
+		return client;
+	}
+
+	public void setClient(User client) {
+		this.client = client;
+	}
+
+	public Payment getPayment() {
+		return payment;
+	}
+
+	public void setPayment(Payment payment) {
+		this.payment = payment;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Order other = (Order) obj;
+		return Objects.equals(id, other.id);
+	}
 }
