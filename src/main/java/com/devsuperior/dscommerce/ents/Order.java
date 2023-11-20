c7752ccc3b28f05e26a7ce3e305af7df2e265fc6
Caue Garcia <cauerg7@outlook.com>
2023-11-20 10:39:40 -0300

    orderitem / orderitemPK

--- src/main/java/com/devsuperior/dscommerce/ents/Order.java
+++ src/main/java/com/devsuperior/dscommerce/ents/Order.java
@@ -1,7 +1,10 @@
 package com.devsuperior.dscommerce.ents;
 
 import java.time.Instant;
+import java.util.HashSet;
+import java.util.List;
 import java.util.Objects;
+import java.util.Set;
 
 import com.devsuperior.dscommerce.ents.enums.OrderStatus;
 
@@ -13,6 +16,7 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.OneToOne;
 import jakarta.persistence.Table;
 
@@ -35,6 +39,9 @@ public class Order {
 	@OneToOne(mappedBy = "order", cascade = CascadeType.ALL)
 	public Payment payment;
 
+	@OneToMany(mappedBy = "id.order")
+	private Set<OrderItem> items = new HashSet<>();
+	
 	public Order(Long id, Instant moment, OrderStatus status, User client, Payment payment) {
 		super();
 		this.id = id;
@@ -83,7 +90,15 @@ public class Order {
 	public void setPayment(Payment payment) {
 		this.payment = payment;
 	}
+	
+	public Set<OrderItem> getItems() {
+		return items;
+	}
 
+	public List<Product> getProducts(){
+		return items.stream().map(x -> x.getProduct()).toList();
+	}
+	
 	@Override
 	public int hashCode() {
 		return Objects.hash(id);
